3a2576b9e8b187f3af96f3ba75a80009f58aef26
ryuhw <lyu9801@naver.com>
2024-12-19 23:46:32 +0900

    Test: 포인트 충전, 사용 동시성 제어 테스트 코드 추가

--- src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -16,7 +17,7 @@ class PointServiceIntegrationTest {
     private PointService pointService;
 
     @Test
-    void 동시에_포인트_충전_5회_요청에_성공한다() throws InterruptedException {
+    void 특정_유저가_동시에_포인트_충전_5회_요청에_성공한다() throws InterruptedException {
         // given
         Long id = 1L;
         Long amount = 10_000L;
@@ -41,6 +42,7 @@ class PointServiceIntegrationTest {
             }
         }));
         latch.await();
+        executorService.shutdown();
 
         // then
         UserPoint userPoint = pointService.point(id);
@@ -48,7 +50,7 @@ class PointServiceIntegrationTest {
     }
 
     @Test
-    void 동시에_포인트_충전_및_사용_5회_요청에_성공한다() throws InterruptedException {
+    void 특정_유저가_동시에_포인트_충전_및_사용_5회_요청에_성공한다() throws InterruptedException {
         // given
         Long id = 2L;
         Long amount = 10_000L;
@@ -73,10 +75,43 @@ class PointServiceIntegrationTest {
             }
         }));
         latch.await();
+        executorService.shutdown();
 
         // then
         UserPoint userPoint = pointService.point(id);
         assertEquals(10_000L, userPoint.point());
+    }
+
+    @Test
+    void 유저_100명이_동시에_포인트_충전_및_사용_요청에_성공한다() throws InterruptedException {
+        // given
+        Long amount = 10_000L;
+        int concurrentRequest = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(concurrentRequest);
+        CountDownLatch latch = new CountDownLatch(concurrentRequest);
+        List<Runnable> tasks = new ArrayList<>();
+        for (Long userId = 1L; userId <= concurrentRequest; userId++) {
+            Long finalUserId = userId;
+            tasks.add(() -> pointService.charge(finalUserId, amount));
+            tasks.add(() -> pointService.use(finalUserId, amount));
+        }
+
+        // when
+        tasks.forEach(task -> executorService.submit(() -> {
+            try {
+                task.run();
+            } finally {
+                latch.countDown();
+            }
+        }));
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        for (Long userId = 1L; userId <= concurrentRequest; userId++) {
+            UserPoint userPoint = pointService.point(userId);
+            assertEquals(0L, userPoint.point());
+        }
     }
 
 }
